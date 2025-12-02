dbf5b173ab0a4c68a9234eab8763646562291e64
Johnolofs <70148035+johnolofs@users.noreply.github.com>
2025-12-02 17:24:02 +0100

    Update run_synaptron.sh

--- Synaptron/run_synaptron.sh
+++ Synaptron/run_synaptron.sh
@@ -23,7 +23,7 @@ set -euo pipefail
 # NAME and GUID are handled in install.sh (not here).
 ###########################################################
 
-REPO_BASE="https://raw.githubusercontent.com/Timpi-official/Nodes/main/Synaptron"
+REPO_BASE="https://raw.githubusercontent.com/johnolofs/timpi/main/Synaptron"
 YML_FILE="docker-compose.yml"
 
 echo "===== Timpi Synaptron – Linux Installer ====="
